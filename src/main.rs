51c94a37107305f952ddfe3736d0eea89373d7c7
Trevor McKay <trs.mckay@gmail.com>
2020-10-23 22:50:32 -0700

    naming and type conformity

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-mod mcu;
+mod emu;
 
 fn main() {
-    println!("\nOTTER-EMU");
-    println!("Version 0.1.0\n");
-
-    let mut mcu = mcu::Otter::new();
-    mcu.load_bin("res/test_all.bin");
-    mcu.run();
+    let bin = "res/programs/leds/leds.bin";
+    emu::emulate(bin);
 }
