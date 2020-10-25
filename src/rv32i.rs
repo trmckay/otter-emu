dc2e8eaf122016ef7166ed1b8f878ebb7df0003e
Trevor McKay <trs.mckay@gmail.com>
2020-10-24 19:16:25 -0700

    overhaul to use fns from bitwise.rs

--- src/rv32i.rs
+++ src/rv32i.rs
@@ -1,5 +1,6 @@
-use std::num::Wrapping;
+#[path = "./bitwise.rs"] mod bitwise;
 
+#[derive(Debug)]
 pub enum Operation {
     // load upper
     LUI, AUIPC,
@@ -21,149 +22,203 @@ pub enum Operation {
 
 pub struct Instruction {
     pub op: Operation,
-    pub rs1: usize,
-    pub rs2: usize,
-    pub rd: usize,
+    pub rs1: u32,
+    pub rs2: u32,
+    pub rd: u32,
     pub imm: u32
 }
 
-// avert your eyes from this monstrosity
-pub fn decode(ir_bytes: u32) -> Instruction {
+fn decode_j_imm(ir_bits: &Vec<bool>) -> u32 {
+    let mut j_imm_vec = bitwise::vec_concat(&vec![ir_bits[31]; 12][..], &ir_bits[12..=19]);
+    j_imm_vec = bitwise::vec_concat(&j_imm_vec[..], &vec![ir_bits[20]; 1]);
+    j_imm_vec = bitwise::vec_concat(&j_imm_vec[..], &ir_bits[21..=30]);
+    j_imm_vec = bitwise::vec_concat(&j_imm_vec[..], &vec![false; 1]);
+    bitwise::vec_to_u32(&j_imm_vec[..])
+}
 
-    // decoded values
-    let opcode: u32 = ir_bytes & 0b00000000000000000000000001111111;
-    let funct3: u32 = (ir_bytes & 0b00000000000000000111000000000000) >> 12;
-    let funct7: u32 = (ir_bytes & 0b11111110000000000000000000000000) >> 25;
+fn decode_b_imm(ir_bits: &Vec<bool>) -> u32 {
+    let mut b_imm_vec = bitwise::vec_concat(&vec![ir_bits[31]; 20][..], &ir_bits[7..=7]);
+    b_imm_vec = bitwise::vec_concat(&b_imm_vec[..], &ir_bits[25..=30]);
+    b_imm_vec = bitwise::vec_concat(&b_imm_vec[..], &ir_bits[8..=11]);
+    b_imm_vec = bitwise::vec_concat(&b_imm_vec[..], &vec![false; 1]);
+    bitwise::vec_to_u32(&b_imm_vec[..])
+}
 
-    // i_imm = {20{ir_bytes[32]}, ir_bytes[30:20]}
-    let mut i_imm = Wrapping(0u32);
-    if (ir_bytes & 0b1000000000000000000000000000) > 0 {
-        i_imm += Wrapping(0b11111111111111111111000000000000u32);
-    }
-    i_imm += Wrapping((ir_bytes & 0b01111111111100000000000000000000u32) >> 20 as u32);
+fn decode_u_imm(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(
+        &bitwise::vec_concat(&ir_bits[12..=31], &[false; 12])[..])
+}
 
-    // s_imm = {20{ir_bytes[31]}, ir_bytes[30, 25], ir_bytes[11:7]}
-    let mut s_imm = Wrapping(0u32);
-    if ir_bytes & ir_bytes & 0b1000000000000000000000000000 > 0 {
-        s_imm += Wrapping(0b11111111111111111111000000000000u32);
-    }
-    s_imm += Wrapping((ir_bytes & 0b01111110000000000000000000000000) >> 20 as u32);
-    s_imm += Wrapping((ir_bytes & 0b00000000000000000000111110000000) >> 7 as u32);
+fn decode_i_imm(ir_bits: &Vec<bool>) -> u32 {
+    let i_imm_vec = bitwise::vec_concat(&vec![ir_bits[31]; 20][..], &ir_bits[20..=30]);
+    bitwise::vec_to_u32(&i_imm_vec[..])
+}
 
-    // u_imm = {ir_bytes[31:12], 12{0}}
-    let u_imm = Wrapping(ir_bytes & 0b11111111111111111111000000000000 as u32);
+fn decode_s_imm(ir_bits: &Vec<bool>) -> u32 {
+    let mut s_imm_vec = bitwise::vec_concat(&vec![ir_bits[31]; 20][..], &ir_bits[25..=30]);
+    s_imm_vec = bitwise::vec_concat(&s_imm_vec[..], &ir_bits[7..=11]);
+    bitwise::vec_to_u32(&s_imm_vec[..])
+}
 
-    // b_imm = {12{ir_bytes[31]}, ir_bytes[7], ir_bytes[30:25], ir_bytes[11:8], 0}
-    let mut b_imm = Wrapping(0u32);
-    if ir_bytes & 0b1000000000000000000000000000 > 0 {
-        b_imm += Wrapping(0b11111111111111111111000000000000 as u32);
-    }
-    b_imm += Wrapping((ir_bytes & 0b00000000000000000000000010000000) << 8 as u32);
-    b_imm += Wrapping((ir_bytes & 0b01111110000000000000000000000000) >> 20 as u32);
-    b_imm += Wrapping((ir_bytes & 0b00000000000000000000111100000000) >> 7 as u32);
+// TODO: turn these into macros
 
-    // j_imm = {12{ir_bytes[31]}, ir_bytes[19:12], ir_bytes[20], ir_bytes[30:21], 0}
-    let mut j_imm = Wrapping(0u32);
-    if ir_bytes & 0b1000000000000000000000000000 > 0 {
-        j_imm += Wrapping(0b11111111111100000000000000000000u32);
-    }
-    j_imm += Wrapping(ir_bytes & 0b00000000000011111111000000000000 as u32);
-    j_imm += Wrapping((ir_bytes & 0b00000000000100000000000000000000) >> 9 as u32);
-    j_imm += Wrapping((ir_bytes & 0b01111111111000000000000000000000) >> 20 as u32);
+fn decode_funct3(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(&ir_bits[12..=14])
+}
 
-    let mut ir = Instruction {
-        op: Operation::Invalid,
-        rs1: ((ir_bytes & 0b00000000000011111000000000000000) >> 15) as usize,
-        rs2: ((ir_bytes & 0b00000001111100000000000000000000) >> 20) as usize,
-        rd:  ((ir_bytes & 0b00000000000000000000111110000000) >> 7) as usize,
-        imm: 0
-    };
+fn decode_funct7(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(&ir_bits[25..=31])
+}
+
+fn decode_rs1(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(&ir_bits[15..=19])
+}
+
+fn decode_rs2(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(&ir_bits[20..=24])
+}
+
+fn decode_rd(ir_bits: &Vec<bool>) -> u32 {
+    bitwise::vec_to_u32(&ir_bits[7..=11])
+}
+
+pub fn decode(ir: u32) -> Instruction {
+    let ir_bits = bitwise::u32_to_vec(ir);
+    let opcode = bitwise::vec_to_u32(&ir_bits[0..=6]);
+
+    let mut op_type = Operation::Invalid;
+    let mut rd = 0;
+    let mut rs1 = 0;
+    let mut rs2 = 0;
+    let mut imm = 0;
 
     // match opcode
     match opcode {
-        0b0110111 => { ir.op = Operation::LUI; ir.imm = u_imm.0},
-        0b0010111 => { ir.op = Operation::AUIPC; ir.imm = u_imm.0},
-        0b1101111 => { ir.op = Operation::JAL; ir.imm = j_imm.0},
-        0b1100111 => { ir.op = Operation::JALR; ir.imm = i_imm.0},
+        // lui
+        0b0110111 => {
+            op_type = Operation::LUI;
+            imm = decode_u_imm(&ir_bits);
+            rd = decode_rd(&ir_bits);
+        },
+        // auipc
+        0b0010111 => {
+            op_type = Operation::AUIPC;
+            imm = decode_u_imm(&ir_bits);
+            rd = decode_rd(&ir_bits);
+        },
+        // jal
+        0b1101111 => {
+            op_type = Operation::JAL;
+            imm = decode_j_imm(&ir_bits);
+            rd = decode_rd(&ir_bits);
+        },
+        // jalr
+        0b1100111 => {
+            op_type = Operation::JALR;
+            imm = decode_i_imm(&ir_bits);
+            rs1 = decode_rs1(&ir_bits);
+            rd = decode_rd(&ir_bits);
+        },
         // branch
         0b1100011 => {
-            ir.imm = b_imm.0;
-            match funct3 {
-                0b000 => ir.op = Operation::BEQ,
-                0b001 => ir.op = Operation::BNE,
-                0b100 => ir.op = Operation::BLT,
-                0b101 => ir.op = Operation::BGE,
-                0b110 => ir.op = Operation::BLTU,
-                0b111 => ir.op = Operation::BGEU,
-                    _ => ir.op = Operation::Invalid
+            rs1 = decode_rs1(&ir_bits);
+            rs2 = decode_rs2(&ir_bits);
+            imm = decode_b_imm(&ir_bits);
+            match decode_funct3(&ir_bits) {
+                0b000 => op_type = Operation::BEQ,
+                0b001 => op_type = Operation::BNE,
+                0b100 => op_type = Operation::BLT,
+                0b101 => op_type = Operation::BGE,
+                0b110 => op_type = Operation::BLTU,
+                0b111 => op_type = Operation::BGEU,
+                    _ => ()
             }
         },
         // load
         0b0000011 => {
-            ir.imm = i_imm.0;
-            match funct3 {
-                0b000 => ir.op = Operation::LB,
-                0b001 => ir.op = Operation::LH,
-                0b010 => ir.op = Operation::LW,
-                0b100 => ir.op = Operation::LBU,
-                0b101 => ir.op = Operation::LHU,
-                    _ => ir.op = Operation::Invalid
+            imm = decode_i_imm(&ir_bits);
+            rs1 = decode_rs1(&ir_bits);
+            rd = decode_rd(&ir_bits);
+            match decode_funct3(&ir_bits) {
+                0b000 => op_type = Operation::LB,
+                0b001 => op_type = Operation::LH,
+                0b010 => op_type = Operation::LW,
+                0b100 => op_type = Operation::LBU,
+                0b101 => op_type = Operation::LHU,
+                    _ => ()
             }
         },
         // store
         0b0100011 => {
-            ir.imm = s_imm.0;
-            match funct3 {
-                0b000 => ir.op = Operation::SB,
-                0b001 => ir.op = Operation::SH,
-                0b101 => ir.op = Operation::SW,
-                    _ => ir.op = Operation::Invalid
+            imm = decode_s_imm(&ir_bits);
+            rs1 = decode_rs1(&ir_bits);
+            rs2 = decode_rs2(&ir_bits);
+            match decode_funct3(&ir_bits) {
+                0b000 => op_type = Operation::SB,
+                0b001 => op_type = Operation::SH,
+                0b010 => op_type = Operation::SW,
+                    _ => ()
             }
         },
         // arithmetic w/ immediate
         0b0010011 => {
-            ir.imm = i_imm.0;
-            match funct3 {
-                0b000 => ir.op = Operation::ADDI,
-                0b010 => ir.op = Operation::SLTI,
-                0b011 => ir.op = Operation::SLTIU,
-                0b100 => ir.op = Operation::XORI,
-                0b110 => ir.op = Operation::ORI,
-                0b111 => ir.op = Operation::ANDI,
-                0b001 => ir.op = Operation::SLLI,
-                0b101 => match funct7 {
-                    0b0000000 => ir.op = Operation::SRLI,
-                    0b0100000 => ir.op = Operation::SRAI,
-                            _ => ir.op = Operation::Invalid
+            imm = decode_i_imm(&ir_bits);
+            rs1 = decode_rs1(&ir_bits);
+            rd = decode_rd(&ir_bits);
+            match decode_funct3(&ir_bits) {
+                0b000 => op_type = Operation::ADDI,
+                0b010 => op_type = Operation::SLTI,
+                0b011 => op_type = Operation::SLTIU,
+                0b100 => op_type = Operation::XORI,
+                0b110 => op_type = Operation::ORI,
+                0b111 => op_type = Operation::ANDI,
+                0b001 => op_type = Operation::SLLI,
+                0b101 => match decode_funct7(&ir_bits) {
+                    0b0000000 => op_type = Operation::SRLI,
+                    0b0100000 => op_type = Operation::SRAI,
+                            _ => ()
                 },
-                    _ => ir.op = Operation::Invalid
+                    _ => ()
             }
         },
         // arithmetic
-        0b0110011 => match funct3 {
-                0b000 => match funct7 {
-                    0b0000000 => ir.op = Operation::ADD,
-                    0b0100000 => ir.op = Operation::SUB,
-                            _ => ir.op = Operation::Invalid
+        0b0110011 => {
+            rs1 = decode_rs1(&ir_bits);
+            rs2 = decode_rs2(&ir_bits);
+            rd = decode_rd(&ir_bits);
+            match decode_funct3(&ir_bits) {
+                0b000 => match decode_funct7(&ir_bits) {
+                    0b0000000 => op_type = Operation::ADD,
+                    0b0100000 => op_type = Operation::SUB,
+                            _ => ()
                 },
-                0b001 => ir.op = Operation::SLL,
-                0b010 => ir.op = Operation::SLT,
-                0b011 => ir.op = Operation::SLTU,
-                0b100 => ir.op = Operation::XOR,
+                0b001 => op_type = Operation::SLL,
+                0b010 => op_type = Operation::SLT,
+                0b011 => op_type = Operation::SLTU,
+                0b100 => op_type = Operation::XOR,
                 0b101 => {
-                    match funct7 {
-                        0b0000000 => ir.op = Operation::SRL,
-                        0b0100000 => ir.op = Operation::SRA,
-                                _ => ir.op = Operation::Invalid
+                    match decode_funct7(&ir_bits) {
+                        0b0000000 => op_type = Operation::SRL,
+                        0b0100000 => op_type = Operation::SRA,
+                                _ => ()
                     }
                 }
-                0b110 => ir.op = Operation::OR,
-                0b111 => ir.op = Operation::AND,
-                    _ => ir.op = Operation::Invalid
+                0b110 => op_type = Operation::OR,
+                0b111 => op_type = Operation::AND,
+                    _ => ()
+            }
         }
-        _ => ir.op = Operation::Invalid
+        _ => ()
     }
-    ir
+
+    
+    Instruction {
+        op: op_type,
+        rs1: rs1,
+        rs2: rs2,
+        rd: rd,
+        imm: imm
+    }
 }
 
 // TODO: this needs more testing; ideally 1-2 tests per instruction
@@ -180,8 +235,9 @@ mod tests {
     0x000000ef: jal x1, 0
     */
 
+ 
     #[test]
-    fn branch() {
+    fn branch1() {
         let ir_bytes: u32 = 0x01001663;
         let ir = decode(ir_bytes);
         assert_eq!(ir.rs1, 0);
@@ -190,24 +246,34 @@ mod tests {
     }
 
     #[test]
-    fn upper() {
+    fn branch2() {
+        let ir_bytes: u32 = 0xfe000ae3;
+        let ir = decode(ir_bytes);
+        println!("got: {:#034b}", ir.imm);
+        println!("exp: {:#034b}", -0xC);
+        assert_eq!(-0xC, ir.imm as i32);
+    }
+
+    #[test]
+    fn lui() {
         let ir_bytes: u32 = 0x0000f0b7;
         let ir = decode(ir_bytes);
-        assert_eq!(ir.rs1, 1);
+        assert_eq!(ir.rd, 1);
         assert_eq!(ir.imm, (0b1111000000000000));
     }
 
     #[test]
-    fn arithm_imm() {
-        let ir_bytes: u32 = 0x07c18f93;
+    fn addi() {
+        // addi x1, x0, 0x1d8
+        let ir_bytes: u32 = 0x1d800093;
         let ir = decode(ir_bytes);
-        assert_eq!(ir.rd, 31);
-        assert_eq!(ir.rs1, 3);
-        assert_eq!(ir.imm, 124);
+        assert_eq!(ir.rd, 1);
+        assert_eq!(ir.rs1, 0);
+        assert_eq!(ir.imm, 0x1d8);
     }
 
     #[test]
-    fn store() {
+    fn sw() {
         let ir_bytes: u32 = 0x00112623;
         let ir = decode(ir_bytes);
         assert_eq!(ir.rs1, 2);
@@ -216,7 +282,7 @@ mod tests {
     }
 
     #[test]
-    fn arithm() {
+    fn add() {
         let ir_bytes: u32 = 0x00f706b3;
         let ir = decode(ir_bytes);
         assert_eq!(ir.rd, 13);
@@ -225,10 +291,22 @@ mod tests {
     }
 
     #[test]
-    fn jump() {
-        let ir_bytes: u32 = 0x000000ef;
+    fn jal() {
+        // jal x31, 0x3d60
+        let ir_bytes: u32 = 0x39903fef;
+        let ir = decode(ir_bytes);
+        println!("got: {:#034b}", ir.imm as u32);
+        println!("exp: {:#034b}", 0x3d60 as u32);
+        assert_eq!(ir.rd, 31);
+        assert_eq!(ir.imm, 0x3d60);
+    }
+
+    #[test]
+    fn jalr() {
+        // jalr x1, 0x1d8
+        let ir_bytes: u32 = 0x1d8100e7;
         let ir = decode(ir_bytes);
         assert_eq!(ir.rd, 1);
-        assert_eq!(ir.imm, 0);
+        assert_eq!(ir.imm, 0x1d8);
     }
 }
