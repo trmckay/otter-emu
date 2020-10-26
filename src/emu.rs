bc2f2bd6d29f3e9aab6aabb152d1028e460cf753
Trevor McKay <trs.mckay@gmail.com>
2020-10-25 22:23:11 -0700

    add flexible program options

--- src/emu.rs
+++ src/emu.rs
@@ -1,80 +1,44 @@
 #[path = "./otter.rs"] mod otter;
+#[path = "./cli.rs"] mod cli;
 use std::{thread, time};
 
-fn refresh_ui(mcu: &otter::MCU, debug: bool) {
-        print!("\x1B[2J\x1B[1;1H"); // clear terminal
+const RF_NAMES: [&str; 32] = [
+    "zero", "ra  ", "sp  ", "gp  ", "tp  ", "t0  ", "t1  ", "t2  ",
+    "s0  ", "s1  ", "a0  ", "a1  ", "a2  ", "a3  ", "a4  ", "a5  ",
+    "a6  ", "a7  ", "s2  ", "s3  ", "s4  ", "s5  ", "s6  ", "s7  ",
+    "s8  ", "s9  ", "s10 ", "s11 ", "t3  ", "t4  ", "t5  ", "t6  "
+];
 
-        println!(" PC: {:#08x}", mcu.pc);
-        println!("+-------------------------------------------+");
-        if debug {
-            let ir = mcu.fetch();
-            println!("      Instruction: {:?} ({:#08x})",
-                ir.op, mcu.mem.rd(mcu.pc, otter::mem::Size::Word));
-            println!("  Operand 1 (rs1): x{:#} = {:#} / {:#x}",
-                ir.rs1, mcu.rf.rd(ir.rs1), mcu.rf.rd(ir.rs1));
-            println!("  Operand 2 (rs2): x{:#} = {:#} / {:#x}",
-                ir.rs2, mcu.rf.rd(ir.rs2), mcu.rf.rd(ir.rs2));
-            println!(" Destination (rd): {:#}", ir.rd);
-            println!("        Immediate: {:#} / {:#x}", ir.imm, ir.imm);
-
-            println!("+-------------------------------------------+");
-
-            println!(" Registers:");
-            for i in 0..32 {
-                let rd = mcu.rf.rd(i);
-                println!("   x{:#02} = {:#} / {:#08x}", i, rd, rd);
-            }
-        }
-
-        println!("+-------------------------------------------+");
-
-        println!("           F E D C B A 9 8 7 6 5 4 3 2 1 0");
-        print!("      LEDs: ");
-        for led in mcu.leds().iter().rev() {
-            if *led {
-                print!("* ");
-            }
-            else {
-                print!("- ");
-            }
-        }
-
-        print!("\n  Switches: ");
-        for led in mcu.switches().iter().rev() {
-            if *led {
-                print!("* ");
-            }
-            else {
-                print!("- ");
-            }
-        }
-        println!("");
-        println!(" 7-Segment: {:#04x}", mcu.sseg());
-
-        println!("+-------------------------------------------+");
-
-
+pub struct Options {
+    pub bin: String,
+    pub debug: bool,
+    pub bps: Vec<u32>,
+    pub log_path: String,
+    pub log_to_f: bool
 }
 
-fn run_cli(mcu: &mut otter::MCU) {
-    let debug = unsafe { super::DEBUG };
-
+fn run_cli(mcu: &mut otter::MCU, opts: &Options) {
     loop {
-        refresh_ui(mcu, debug);
+        if opts.bps.contains(&mcu.pc) && !opts.debug {
+            println!("\nHit breakpoint {:#010X}\nPress enter to start debug mode", mcu.pc);
+        }
 
-        if debug {
+        thread::sleep(time::Duration::from_millis(2));
+
+        let ir = mcu.fetch();
+        cli::refresh_ui(&mcu, &ir, opts);
+
+        if opts.debug {
             let mut line = String::new();
             std::io::stdin().read_line(&mut line).unwrap();
         }
 
-        // exec 1
-        mcu.step();
-        thread::sleep(time::Duration::from_millis(2));
+        mcu.exec(ir.0);
     }
 }
 
-pub fn emulate(bin: &str) {
+pub fn emulate(opts: &Options) {
     let mut mcu = otter::MCU::new();
-    mcu.load_bin(bin);
-    run_cli(&mut mcu);
+    mcu.load_bin(&opts.bin);
+    run_cli(&mut mcu, opts);
 }
